70f26773795489576571b50c5ab93f57307c5cf2
Qamrul-Hassan <sajal700@gmail.com>
2024-10-14 00:46:44 +0600

    massage

---
+++ Q7.js
@@ -0,0 +1,7 @@
+/* 7. Remove Whitespace from a String.
+Write a function that all the leading and trailing whitespace from string. */
+
+function trimString(str){
+    return str.trim();
+}
+console.log(trimString("Hello! World"));
