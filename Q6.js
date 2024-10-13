98c33a721863243e6e65f4e2b9450e82c8f4ad3d
Qamrul-Hassan <sajal700@gmail.com>
2024-10-14 00:43:32 +0600

    massage

---
+++ Q6.js
@@ -0,0 +1,7 @@
+/* 6. Convert a String to Uppercase
+Write a function that converts all lowercase letters in a string to uppercase. */
+
+function toUpperCase(str){
+    return str.toUpperCase();
+}
+console.log(toUpperCase("qamrul"));
